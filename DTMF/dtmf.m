4777a48ff86ef5c4265360bb8abcb1191fd584b9
jiegec <noc@jiegec.ac.cn>
2020-01-11 00:32:41 +0800

    Add dtmf using fft

--- DTMF/dtmf.m
+++ DTMF/dtmf.m
@@ -8,12 +8,21 @@ t = Fs*(0:len-1)/len;
 % plot(t, abs(freq));
 
 f = [697, 770, 852, 941, 1209, 1336, 1477, 1633];
-res = [[0,0]];
+res = [];
 for i = 1:length(f)
     val = my_goertzel(y, Fs, f(i));
     res = [res; [val, f(i)]];
 end
 
 res = sortrows(res, 1, 'descend');
-ans1 = res(1,2)
-ans2 = res(2,2)
+ans_go1 = res(1,2)
+ans_go2 = res(2,2)
+
+res2 = [];
+for i = 1:length(f)
+    val = freq(round(f(i)/Fs*len));
+    res2 = [res2; [val, f(i)]];
+end
+res2 = sortrows(res2, 1, 'descend');
+ans_fft1 = res2(1,2)
+ans_fft2 = res2(2,2)
