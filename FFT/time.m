930d1fff79976907f66bf905398afdbae637307b
jiegec <noc@jiegec.ac.cn>
2020-01-11 09:36:24 +0800

    Add more timing

--- FFT/time.m
+++ FFT/time.m
@@ -1,17 +1,28 @@
-n = 10240;
-L = 16;
-N = 20480;
+n = 4096;
+m = 4096;
 data1 = (1:n);
-data2 = (1:n);
+data2 = (1:m);
 
 ans1 = direct(data1, data2);
 ans2 = conv_fft(data1, data2);
-ans3 = overlap_add(data1, data2, L);
-ans4 = overlap_save(data1, data2, N);
+ans3 = overlap_add(data1, data2, n);
+ans4 = overlap_add(data1, data2, n/16);
+ans5 = overlap_add(data1, data2, n/256);
+ans6 = overlap_add(data1, data2, n/4096);
+ans7 = overlap_save(data1, data2, m);
+ans8 = overlap_save(data1, data2, m*2);
+ans9 = overlap_save(data1, data2, m*3);
+ans10 = overlap_save(data1, data2, m*4);
 
 assert(mean(ans1-ans2) < 1e-4);
 assert(mean(ans1-ans3) < 1e-4);
 assert(mean(ans1-ans4) < 1e-4);
+assert(mean(ans1-ans5) < 1e-4);
+assert(mean(ans1-ans6) < 1e-4);
+assert(mean(ans1-ans7) < 1e-4);
+assert(mean(ans1-ans8) < 1e-4);
+assert(mean(ans1-ans9) < 1e-4);
+assert(mean(ans1-ans10) < 1e-4);
 
 f1 = @() direct(data1, data2);
 timeit(f1)
@@ -19,8 +30,26 @@ timeit(f1)
 f2 = @() conv_fft(data1, data2);
 timeit(f2)
 
-f3 = @() overlap_add(data1, data2, L);
+f3 = @() overlap_add(data1, data2, n);
 timeit(f3)
 
-f4 = @() overlap_save(data1, data2, N);
+f4 = @() overlap_add(data1, data2, n/16);
 timeit(f4)
+
+f5 = @() overlap_add(data1, data2, n/256);
+timeit(f5)
+
+f6 = @() overlap_add(data1, data2, n/4096);
+timeit(f6)
+
+f7 = @() overlap_save(data1, data2, m);
+timeit(f7)
+
+f8 = @() overlap_save(data1, data2, m*2);
+timeit(f8)
+
+f9 = @() overlap_save(data1, data2, m*3);
+timeit(f9)
+
+f10 = @() overlap_save(data1, data2, m*4);
+timeit(f10)
