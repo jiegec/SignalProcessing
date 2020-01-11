19cfcfbbfb5a48725c01c285d399d8a7f564de71
jiegec <noc@jiegec.ac.cn>
2020-01-11 14:49:44 +0800

    Use filters

--- FDM/fdm.m
+++ FDM/fdm.m
@@ -15,3 +15,19 @@ L = 4;
 up_Fs = Fs * L;
 
 up1 = upsample(y1, L);
+filter1 = fir1(length(up1), 0.25);
+up_f1 = conv(up1, filter1);
+
+up2 = upsample(y2, L);
+filter2 = fir1(length(up2), [0.25 0.5]);
+up_f2 = conv(up2, filter2);
+
+up3 = upsample(y3, L);
+filter3 = fir1(length(up3), [0.5 0.75]);
+up_f3 = conv(up3, filter3);
+
+up4 = upsample(y4, L);
+filter4 = fir1(length(up4), 0.75, 'high');
+up_f4 = conv(up4, filter4);
+
+res = up_f1 + up_f2 + up_f3 + up_f4;
